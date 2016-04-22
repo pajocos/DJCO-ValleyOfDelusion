9aaab6512001f2b8cb3053ea9f5d478ba0ebc432
Pedro Castro <ei12019@fe.up.pt>
2016-04-22 18:10:42 +0100

    enemy

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Utils;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    private Rigidbody rgd;
+    private int patternIterator = -1;
+    private float movementLeft = 0;
+    private List<Movement> movementPattern;
+    private float lastAng;
+	// Use this for initialization
+	void Start () {
+
+        movementPattern = new List<Movement>();
+        movementPattern.Add(new Movement(5f, false, Vector3.left));
+        movementPattern.Add(new Movement(Mathf.PI, true, Vector3.up));
+        movementPattern.Add(new Movement(5f, false, Vector3.right));
+        movementPattern.Add(new Movement(Mathf.PI, true, Vector3.up));
+
+        rgd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+        if (movementLeft > 0) {
+
+           
+
+            if (movementPattern[patternIterator].isRotation()) {
+
+
+                rgd.AddTorque(movementPattern[patternIterator].getAxis(),ForceMode.VelocityChange);
+
+                float ang;
+                Vector3 ve;
+                GetComponent<Transform>().rotation.ToAngleAxis(out ang,out ve);
+                Debug.Log("ang:" + ang + "last:" + lastAng  + "dif" + (ang- lastAng));
+
+                movementLeft -= Time.deltaTime;
+                lastAng = ang;
+            } else {
+
+                rgd.velocity = movementPattern[patternIterator].getAxis() * 2;
+                movementLeft -= Time.deltaTime;
+
+
+
+            }
+
+
+
+
+
+
+
+
+        } else {
+
+            rgd.velocity = Vector3.zero;
+            rgd.angularVelocity = Vector3.zero;
+
+
+
+
+            patternIterator++;
+            if (patternIterator >= movementPattern.Count) {
+                patternIterator = 0;
+            }
+            movementLeft = movementPattern[patternIterator].getValue();
+
+            float ang;
+            Vector3 ve;
+            GetComponent<Transform>().rotation.ToAngleAxis(out ang, out ve);
+
+            lastAng = ang;
+
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Utils/Movement.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace Assets.Scripts.Utils {
+
+
+    public class Movement {
+
+
+        private float value;
+        private bool rotation;
+        private Vector3 axis;
+
+        public Movement(float val, bool rot, Vector3 axis) {
+            this.value = val;
+            this.rotation = rot;
+            this.axis = axis;
+        }
+
+        public Vector3 getAxis() {
+            return axis;
+        }
+
+        public float getValue() {
+            return value;
+        }
+
+        public bool isRotation() {
+            return rotation;
+        }
+    }
+
+}
