1aac5704cacd133470abffc8229f31d10d9fa6ad
Inframan <ei12087@fe.up.pt>
2016-05-09 12:28:58 +0100

    improving jump little by little

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,13 +2,23 @@
 using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
+
+    private PlayerMovement movement;
+
 	// Use this for initialization
 	void Start () {
+        movement = GetComponent<PlayerMovement>();
 	
 	}
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        bool jump = Input.GetButtonDown("Jump");
+        movement.Move(moveHorizontal, moveVertical, jump);
+
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 10f;
     public float AngularSpeed = 10f;
+    public float Gravity = 10f;
 
-
-    public float JumpDistance = 1000f;
     public float JumpSpeed = 10f;
-    private bool jumping = false;
+
+    private Vector3 internalVelocity = Vector3.zero;
+
 
     Rigidbody rigidbody;
     
@@ -19,45 +20,41 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
     }
 
-    void FixedUpdate()
+    public void Move(float turnAngle, float speedInput, bool jump)
     {
-        movePlayer();
-    }
-
-    private void movePlayer()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        sum += moveHorizontal * AngularSpeed * Time.deltaTime;
-        Vector3 movement = new Vector3(Mathf.Sin((sum * Mathf.PI) / 180), 0.0f, Mathf.Cos((Mathf.PI * sum) / 180));
-
-        rigidbody.velocity = movement * Speed * moveVertical;
-
-        rigidbody.position = rigidbody.position + rigidbody.velocity * Time.deltaTime;
-
-        rigidbody.rotation = Quaternion.Euler(0f, sum, 0f);
+        rotationManager(turnAngle);
+        movementManager(speedInput);
+        jumpManager(jump);
         
 
-        if (Input.GetButtonDown("Jump") && grounded)
-            jumping = true;
-
-        MyJump();
+        rigidbody.position = rigidbody.position + internalVelocity * Time.deltaTime;
     }
 
-    private void MyJump()
+    private void movementManager(float speedInput)
     {
-        if (jumping)
+        Vector3 movement = new Vector3(Mathf.Sin((sum * Mathf.PI) / 180), 0.0f, Mathf.Cos((Mathf.PI * sum) / 180));
+
+        internalVelocity = movement * Speed * speedInput;
+    }
+
+    private void rotationManager(float turnAngle)
+    {
+        sum += turnAngle * AngularSpeed * Time.deltaTime;
+        rigidbody.rotation = Quaternion.Euler(0f, sum, 0f);
+    }
+
+    private void jumpManager(bool jump)
+    {
+        if (jump && grounded)
         {
-            if (transform.position.y >= JumpDistance)
-                jumping = false;
-            else
-                rigidbody.velocity += Vector3.up * JumpSpeed;
+            internalVelocity += Vector3.up * JumpSpeed;
+
         }
-        else if(!grounded)
-            rigidbody.velocity -= Vector3.up * JumpSpeed;
+        if (!grounded)
+            internalVelocity -= Vector3.up* Gravity * Time.deltaTime;
     }
 
 
@@ -66,6 +63,7 @@ public class PlayerMovement : MonoBehaviour
         if(col.collider.tag.Equals("Floor"))
         {
             grounded = true;
+            internalVelocity = Vector3.zero;
         }
     }    
 
