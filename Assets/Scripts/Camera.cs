93c0f7c0664b4e2f2d8eda20237b24d9af620caf
Paulo Costa <pajocos@gmail.com>
2016-05-09 19:53:28 +0100

    prevent cross walls
    code commented

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class Camera : MonoBehaviour
+{
 
     public GameObject player;
     private float disMax, disCurrent;
 
-	void Start () {
+    void Start()
+    {
         disMax = 10;
-	}
-	
-	void Update () {
-        transform.RotateAround(player.transform.position, player.transform.up, Input.GetAxis("Mouse X") * 90 * Time.deltaTime);
-        transform.RotateAround(player.transform.position, player.transform.right, Input.GetAxis("Mouse Y") * 90 * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        transform.RotateAround(player.transform.position, player.transform.up, Input.GetAxis("Mouse X") * 45 * Time.deltaTime);
+        transform.RotateAround(player.transform.position, player.transform.right, Input.GetAxis("Mouse Y") * 45 * Time.deltaTime);
 
         disCurrent = Vector3.Distance(player.transform.position, transform.position);
         transform.LookAt(player.transform);
         if (disCurrent > disMax || disMax < -disMax)
-        {
             transform.position = Vector3.Lerp(transform.position, transform.position + transform.forward * disMax, Time.deltaTime);
-        }
 
-        //struct Quaternion = Quaternion.FromToRotation(player.forward, transform.forward) * transform.rotation;
 
-        //transform.rotation.eulerAngles.y = Quaternion.Lerp(transform.rotation, RotacaoRefCam, Time.deltaTime).eulerAngles.y;
-    }
+
+        //PREVENIR ATRAVESSAR PAREDES e CHAO...
+    //    RaycastHit colisionRay;
+
+    //    if (Physics.Raycast(transform.position, transform.forward, out colisionRay))
+    //    {
+    //        if (colisionRay.transform != player.transform)
+    //        {
+    //            transform.position = colisionRay.point + transform.forward * 2;
+    //        }
+
+    //    }
+    //}
 }
