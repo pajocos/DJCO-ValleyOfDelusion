704f83da1e71ed9b384b88b880130a16106feb68
Paulo Costa <pajocos@gmail.com>
2016-05-17 15:42:17 +0100

    camera updated

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,6 +5,8 @@ public class Camera : MonoBehaviour
 
     public GameObject player;
     private float disMax, disCurrent;
+    private Quaternion initialRotation;
+    private Vector3 initialPos;
 
     void Start()
     {
@@ -13,9 +15,25 @@ public class Camera : MonoBehaviour
 
     void Update()
     {
-        transform.RotateAround(player.transform.position, player.transform.up, Input.GetAxis("Mouse X") * 45 * Time.deltaTime);
-        transform.RotateAround(player.transform.position, player.transform.right, Input.GetAxis("Mouse Y") * 45 * Time.deltaTime);
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialRotation = transform.rotation;
+            initialPos = transform.position;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            transform.RotateAround(player.transform.position, player.transform.up,
+                Input.GetAxis("Mouse X") * 45 * Time.deltaTime);
+            transform.RotateAround(player.transform.position, player.transform.right,
+                Input.GetAxis("Mouse Y") * 45 * Time.deltaTime);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            transform.position = initialPos;
+            transform.rotation = initialRotation;
+            //transform.position = Vector3.Lerp(transform.position,initialPos, Time.deltaTime);
+            //transform.rotation = Quaternion.Lerp(transform.rotation,initialRotation, Time.deltaTime);
+        }
         disCurrent = Vector3.Distance(player.transform.position, transform.position);
         transform.LookAt(player.transform);
         if (disCurrent > disMax || disMax < -disMax)
