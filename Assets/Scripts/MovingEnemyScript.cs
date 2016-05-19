5dde150ff8bbe3f9ae0da8dd425ca1560fe7e679
Inframan <ei12087@fe.up.pt>
2016-05-19 18:50:11 +0100

    enemy moving and chasing player if in range

--- Assets/Scripts/MovingEnemyScript.cs
+++ Assets/Scripts/MovingEnemyScript.cs
@@ -5,11 +5,42 @@ using System.Collections;
 public class MovingEnemyScript : MonoBehaviour
 {
 
-    public Transform goal;
+    public PlayerBehaviour Player;
+    public Transform[] PatrolPoints;
+
+    public float SensePlayer = 15f;
+
+    private NavMeshAgent agent;
+    private int destPoint;
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+        destPoint = 0;
+        GoToNextPoint();
+    }
+
+    private void GoToNextPoint()
+    {
+        if (PatrolPoints.Length == 0)
+            return;
+
+        destPoint = (destPoint + 1) % PatrolPoints.Length;
+
+        agent.destination = PatrolPoints[destPoint].position;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, agent.destination) < 1f)
+        {
+            GoToNextPoint();
+        } else
+        if (Vector3.Distance(transform.position, Player.transform.position) < SensePlayer)
+        {
+            agent.destination = Player.transform.position;
+
+        }
     }
 }
