85860a98e26d5921c457bd4d8dd9cfb6ee2cbf4f
Inframan <ei12087@fe.up.pt>
2016-06-04 18:00:03 +0100

    finaly moving in this piece of :poop: isn't a horrible traumatizing experience

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerBehaviour : MonoBehaviour {
-
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Collider groundCollider;
     public CharacterMovement movement;
 
     private int floorContacts = 0;
@@ -15,7 +16,8 @@ public class PlayerBehaviour : MonoBehaviour {
     private int gems = 0;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         movement = GetComponent<CharacterMovement>();
         transform = GetComponent<Transform>();
 
@@ -33,32 +35,95 @@ public class PlayerBehaviour : MonoBehaviour {
             movement.Move(moveHorizontal, moveVertical, jump);
         }
 
-        Debug.DrawRay(transform.position + Vector3.up, Vector3.down * dist, Color.red);
-        if (Physics.Raycast(transform.position + Vector3.up, Vector3.down,out hit, dist)) {
+        /*     Debug.DrawRay(transform.position + Vector3.up, Vector3.down * dist, Color.red);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f*Vector3.forward) * dist, Color.blue);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * Vector3.back) * dist, Color.blue);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * Vector3.left) * dist, Color.blue);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * Vector3.right) * dist, Color.blue);
 
-            if (hit.collider.tag == "Floor") {
-                if (!movement.grounded && movement.jumpSpeed < 0 ) {
-                    movement.grounded = true;
-                    movement.jumpSpeed = 0;
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f *( Vector3.right + Vector3.forward)) * dist, Color.cyan);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * (Vector3.right + Vector3.back)) * dist, Color.cyan);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * (Vector3.left + Vector3.forward)) * dist, Color.cyan);
+             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * (Vector3.left + Vector3.back)) * dist, Color.cyan);
 
-                }
-            }
-            
-        } else {
-            movement.grounded = false;
-        }
+             if ( Physics.Raycast(transform.position + Vector3.up, Vector3.down + Vector3.forward, out hit, dist)   
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.forward, out hit, dist)
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.back, out hit, dist)
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.left, out hit, dist)
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.right, out hit, dist)
+
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.right + Vector3.forward), out hit, dist)
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.right + Vector3.back), out hit, dist)
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.left + Vector3.forward), out hit, dist)
+                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.left + Vector3.back), out hit, dist)
+                 ) {
+
+                 if (hit.collider.tag == "Floor") {
+                     if (!movement.grounded && movement.jumpSpeed < 0 ) {
+                         movement.grounded = true;
+                         movement.jumpSpeed = 0;
+
+                     }
+                 }
+
+             } else {
+                 movement.grounded = false;
+             }*/
 
     }
 
+
     void OnCollisionEnter(Collision col)
     {
-        if(col.collider.tag == "Gem")
+        if (col.collider.tag == "Gem")
         {
             gems++;
             col.collider.GetComponentInParent<SphereCollider>().enabled = false;
             col.collider.GetComponentInParent<MeshRenderer>().enabled = false;
 
         }
-    }
         
+        if (col.collider.tag == "Floor")
+        {
+            foreach (ContactPoint c in col.contacts)
+            {
+                if(c.thisCollider == groundCollider)
+                {
+                    movement.grounded = true;
+                    movement.jumpSpeed = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+
+    void OnCollisionStay(Collision col)
+    {   
+
+        if (col.collider.tag == "Floor")
+        {
+            foreach (ContactPoint c in col.contacts)
+            {
+                if (c.thisCollider == groundCollider)
+                {
+                    movement.grounded = true;
+                    movement.jumpSpeed = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.collider.tag == "Floor")
+        {
+            movement.grounded = false;
+        }
+    }
+
 }
+
+
+
