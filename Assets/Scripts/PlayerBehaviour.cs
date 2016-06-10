46197fc5837327a7669fa6f63b5bc97569f8a266
Inframan <ei12087@fe.up.pt>
2016-06-10 17:27:29 +0100

    cleared dead code, added gem getter

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -34,42 +34,6 @@ public class PlayerBehaviour : MonoBehaviour
             bool jump = Input.GetButtonDown("Jump");
             movement.Move(moveHorizontal, moveVertical, jump);
         }
-
-        /*     Debug.DrawRay(transform.position + Vector3.up, Vector3.down * dist, Color.red);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f*Vector3.forward) * dist, Color.blue);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * Vector3.back) * dist, Color.blue);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * Vector3.left) * dist, Color.blue);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * Vector3.right) * dist, Color.blue);
-
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f *( Vector3.right + Vector3.forward)) * dist, Color.cyan);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * (Vector3.right + Vector3.back)) * dist, Color.cyan);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * (Vector3.left + Vector3.forward)) * dist, Color.cyan);
-             Debug.DrawRay(transform.position + Vector3.up, (Vector3.down + 0.25f * (Vector3.left + Vector3.back)) * dist, Color.cyan);
-
-             if ( Physics.Raycast(transform.position + Vector3.up, Vector3.down + Vector3.forward, out hit, dist)   
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.forward, out hit, dist)
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.back, out hit, dist)
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.left, out hit, dist)
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * Vector3.right, out hit, dist)
-
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.right + Vector3.forward), out hit, dist)
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.right + Vector3.back), out hit, dist)
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.left + Vector3.forward), out hit, dist)
-                 || Physics.Raycast(transform.position + Vector3.up, Vector3.down + 0.25f * (Vector3.left + Vector3.back), out hit, dist)
-                 ) {
-
-                 if (hit.collider.tag == "Floor") {
-                     if (!movement.grounded && movement.jumpSpeed < 0 ) {
-                         movement.grounded = true;
-                         movement.jumpSpeed = 0;
-
-                     }
-                 }
-
-             } else {
-                 movement.grounded = false;
-             }*/
-
     }
 
 
@@ -121,6 +85,12 @@ public class PlayerBehaviour : MonoBehaviour
         {
             movement.grounded = false;
         }
+    }
+
+
+    public int getGems()
+    {
+        return gems;
     }
 
 }
