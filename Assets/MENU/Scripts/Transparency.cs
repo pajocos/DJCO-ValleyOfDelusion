16eb8194f4b79605b65f5f919cde9ec437cae826
Paulo Costa <pajocos@gmail.com>
2016-06-17 02:07:30 +0100

    menu started. already moving

---
+++ Assets/MENU/Scripts/Transparency.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Transparency : MonoBehaviour
+{
+    private Image ButtonBackground;
+    public float transparency;
+
+    // Use this for initialization
+    void Start()
+    {
+        ButtonBackground = GetComponent<Image>();
+        SetTransparency(transparency);
+    }
+
+    public void SetTransparency(float t)
+    {
+        //transparency is a value in the [0,1] range
+        Color color = ButtonBackground.color;
+        color.a = t;
+        ButtonBackground.color = color;
+    }
+}
