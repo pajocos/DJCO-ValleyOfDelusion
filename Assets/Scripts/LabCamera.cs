c4614e0faaa35d280a56cb969ba070c20afbda9f
Paulo Costa <pajocos@gmail.com>
2016-06-21 19:47:27 +0100

    lab scene camera

---
+++ Assets/Scripts/LabCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabCamera : MonoBehaviour
+{
+    public float height = 50;
+    public Transform playerTransform;
+    private Vector3 pos;
+
+    void Start ()
+    {
+        pos = playerTransform.position;
+        transform.position = new Vector3(pos.x, pos.y + height , pos.z);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + height, playerTransform.position.z);
+    }
+}
