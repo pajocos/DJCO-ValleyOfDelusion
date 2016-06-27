355e46ce7546c8fc942267f51a5ccd4fe3b803b2
Inframan <ei12087@fe.up.pt>
2016-06-27 18:01:56 +0100

    pendulum noises

---
+++ Assets/Scripts/PendulumReproducerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PendulumReproducerScript : MonoBehaviour
+{
+
+    private AudioSource pendulumSound;
+
+    void Start()
+    {
+        pendulumSound = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "MusicDetector" && !pendulumSound.isPlaying)
+        {
+            pendulumSound.Play();
+        }
+    }
+}
