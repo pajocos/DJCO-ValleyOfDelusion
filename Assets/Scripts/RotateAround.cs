78adb0926e713f87867c1efbbdb46d7455d3fdd7
Paulo Costa <pajocos@gmail.com>
2016-06-28 02:27:40 +0100

    start vilan

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -3,12 +3,22 @@ using System.Collections;
 
 public class RotateAround : MonoBehaviour
 {
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        //renderer.material.color =
+        //    new Color(1.0f, 1.0f, 0.5f, 1.0f);
+
         if (PlayerBehaviour.gems == 0)
         {
-            transform.RotateAround(transform.position, Vector3.zero, Time.deltaTime*90f);
+            transform.RotateAround(transform.parent.position, new Vector3(0, 2, 0), Time.deltaTime*45f);
         }
         else if (PlayerBehaviour.gems == 1)
         {
