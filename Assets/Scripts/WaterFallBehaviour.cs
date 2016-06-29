1cb8adbd29741121aafb466c357b748bf96a96e7
Inframan <ei12087@fe.up.pt>
2016-06-29 21:48:19 +0100

    final scene

---
+++ Assets/Scripts/WaterFallBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFallBehaviour : MonoBehaviour {
+
+    public float delayTime = 9f;
+    public AudioSource DelaySound;
+	// Use this for initialization
+	void Awake () {
+        DelaySound.PlayDelayed(delayTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
